f3a9b8e80abab2166148f45b11eabbc195b50dea
clap322 <pronn63@gmail.com>
2026-04-18 19:38:33 +0300

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -1,1 +1,63 @@
 from pygame import *
+#класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+#конструктор класса
+  def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+      #вызываем конструктор класса (Sprite):
+      sprite.Sprite.__init__(self)
+      #каждый спрайт должен хранить свойство image - изображение
+      self.image = transform.scale(image.load(player_image), (width, height))
+      self.speed = player_speed
+      #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+      self.rect = self.image.get_rect()
+      self.rect.x = player_x
+      self.rect.y = player_y
+#метод, отрисовывающий героя на окне
+  def reset(self):
+      window.blit(self.image, (self.rect.x, self.rect.y))
+#класс главного игрока
+class Player(GameSprite):
+  #метод для управления спрайтом стрелками клавиатуры
+    def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 150:
+           self.rect.y += self.speed
+    def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 150:
+           self.rect.y += self.speed       
+
+
+
+back = (200, 255, 255)
+win_width=600
+win_height=500
+window=display.set_mode((win_width, win_height))
+window.fill(back)
+game=True
+finish=False
+clock=time.Clock()
+FPS=60
+
+racket1=Player('racket.png', 30, 200, 4, 50, 150)
+racket2=Player('racket.png', 520, 200, 4, 50, 150)
+ball=GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+
+while game:
+    for e in event.get():
+        if e.type==QUIT :
+            game=False
+        if finish != True:
+            window.fill(back)
+            racket1.update_l()
+            racket2.update_r()
+
+            racket1.reset()
+            racket2.reset()
+        display.update()
+        clock.tick(FPS)
