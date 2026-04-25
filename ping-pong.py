04844409d8f8d935d61e0be7a579f183e1a242ee
clap322 <pronn63@gmail.com>
2026-04-25 18:29:15 +0300

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -47,6 +47,9 @@ racket1=Player('racket.png', 30, 200, 4, 50, 150)
 racket2=Player('racket.png', 520, 200, 4, 50, 150)
 ball=GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
+speed_x=3
+speed_y=3
+
 
 while game:
     for e in event.get():
@@ -56,7 +59,12 @@ while game:
             window.fill(back)
             racket1.update_l()
             racket2.update_r()
-
+            ball.rect.x+=speed_x
+            ball.rect.y+=speed_y
+            if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+                speed_x*=-1
+            if ball.rect.y>win_height-50 or ball.rect.y<0:
+                speed_y*=-1
             racket1.reset()
             racket2.reset()
         display.update()
